5b9adf6e2b30cd48718945fbe09740bc8ef0586f
Jerhynh <60614943+Jerhynh@users.noreply.github.com>
2022-03-30 15:20:49 -0500

    Implement docs for base vars and ctor.

---
+++ Src/NyaaNet/APIClient.cs
@@ -0,0 +1,34 @@
+﻿namespace NyaaNet;
+
+/// <summary>
+/// APIClient contains all externally invokable methods used to interact with the NYAA-API-GO.
+/// </summary>
+public class APIClient
+{
+    /// <summary>
+    /// The address or "URI" of the server.
+    /// </summary>
+    public string ServerURI { get; set; }
+
+    /// <summary>
+    /// The APIToken or authorization token supplied by the server to allow for use of elevated API endpoints.
+    /// </summary>
+    public string APIToken { get; set; }
+
+    /// <summary>
+    /// Default ctor for the APIClient class.
+    /// </summary>
+    /// <param name="ServerURI"></param>
+    /// <param name="APIToken"></param>
+    /// <exception cref="ArgumentNullException"></exception>
+    public APIClient(string ServerURI, string APIToken)
+    {
+        this.ServerURI = ServerURI ?? throw new ArgumentNullException(nameof(ServerURI));
+        this.APIToken = APIToken ?? throw new ArgumentNullException(nameof(APIToken));
+        if (this.ServerURI.EndsWith("/")) // strip '/' from rnd of URI to prevent URI mutilation.
+            // Typically I would remove the ending '/' char like this `ServerURI = ServerURI.Substring(0, ServerURI.Length - 1);` but VS suggested this unreadable cleaner way.
+            this.ServerURI = this.ServerURI[..(ServerURI.Length - 1)]; 
+    }
+
+
+}
