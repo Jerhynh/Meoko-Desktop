ddfec91ef3444cfdb00b7b7a5b1809eb3d5826bb
Jerhynh <60614943+Jerhynh@users.noreply.github.com>
2022-04-12 07:46:47 -0500

    Implement API functions for cat and subcat search

--- Src/NyaaNetTesting/Program.cs
+++ Src/NyaaNetTesting/Program.cs
@@ -12,8 +12,13 @@ namespace NyaaNetTesting
         static async Task Main()
         {
             Console.Title = $"NyaaNet Testing Utility | Version: {Assembly.GetExecutingAssembly().GetName().Version}";
-            //APIClient client = new("http://127.0.0.1:8080/","admin", "pass1234");
-            //Console.WriteLine(await client.HealthCheckAsync());
+            APIClient client = new("http://127.0.0.1:8080/", "admin", "pass1234");
+            Console.WriteLine(await client.HealthCheckAsync());
+            URLBuilder builder = new("http://127.0.0.1:8080/");
+            var url = builder.BuildSearchURL(RouteEndpoints.Anime, AnimeSubCategories.eng, "konosuba",SortingMethods.size, 1, OrderMethods.Descending);
+            Console.WriteLine(url);
+            Console.WriteLine("Sending request...");
+            Console.WriteLine(await client.SendRequestPayloadAsync(url));
             Console.ReadLine();
         }
     }
